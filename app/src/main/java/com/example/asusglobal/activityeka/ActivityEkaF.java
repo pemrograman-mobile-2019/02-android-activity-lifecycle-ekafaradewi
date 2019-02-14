32760b4bac713d26ca1d66860d2d9f9dd1a2a2a5
ekafaradewi <44692821+ekafaradewi@users.noreply.github.com>
2019-02-14 15:56:28 +0700

    ActivityEka

---
+++ app/src/main/java/com/example/asusglobal/activityeka/ActivityEkaF.java
@@ -0,0 +1,13 @@
+package com.example.asusglobal.activityeka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEkaF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eka_f);
+    }
+}
